208898fbe938ee162e4fe35c9881a043b8707059
Brayton Hall <braytonhall@gmail.com>
2020-08-08 22:30:58 -0400

    readded value

--- src/index.js
+++ src/index.js
@@ -59,7 +59,7 @@ const [currNumber, addNumber] = React.useState(0)
     Name:
     <input type="text" name="name" />
   </label>
-  <input type="submit" ="Submit" />
+  <input type="submit" value="Submit" />
     </form>
   </div>
   // need to add .js with controlled component for form
